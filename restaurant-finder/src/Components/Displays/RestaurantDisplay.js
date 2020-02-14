243181dd98b9fd85059b6c367ff007e2882e7118
Dylan Morozowski <dymacski@gmail.com>
2020-02-13 23:53:45 -0500

    successfully displaying restaurant api

--- restaurant-finder/src/Components/Displays/RestaurantDisplay.js
+++ restaurant-finder/src/Components/Displays/RestaurantDisplay.js
@@ -3,21 +3,47 @@ import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
+import CardActions from "@material-ui/core/CardActions";
+import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 
 const RestaurantDisplay = props => {
+  console.log(props.eatery.restaurant);
   return (
-    // <Card>
-    //   <CardHeader>{props.eatery.name}</CardHeader>
-    //   <CardMedia src={props.eatery.featured_image} />
-    //   <CardContent>{props.eatery.location.address}</CardContent>
-    //   <CardContent>{props.eatery.cuisines}</CardContent>
-    //   <CardContent>{props.eatery.user_rating.aggregate_rating}</CardContent>
-    //   <Button href={props.eatery.menu_url} target="blank">
-    //     Menu
-    //   </Button>
-    // </Card>
-    <h1>{props.eatery.name}</h1>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "center"
+      }}
+    >
+      <Card
+        // key={props.eatery.restaurant.id}
+        style={{
+          height: "18em",
+          width: "20em"
+        }}
+      >
+        <CardContent>
+          <Typography>Restaurant: {props.eatery.restaurant.name}</Typography>
+        </CardContent>
+        <CardContent>
+          Address: {props.eatery.restaurant.location.address}
+        </CardContent>
+        <CardContent>
+          Type of Cuisine: {props.eatery.restaurant.cuisines}
+        </CardContent>
+        <CardContent>
+          Rating: {props.eatery.restaurant.user_rating.aggregate_rating}
+        </CardContent>
+        <CardActions style={{ display: "flex", justifyContent: "center" }}>
+          <Button href={props.eatery.restaurant.menu_url} target="blank">
+            Menu
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
   );
 };
 
--- restaurant-finder/src/Components/Displays/Restaurants.js
+++ restaurant-finder/src/Components/Displays/Restaurants.js
@@ -6,8 +6,20 @@ class Restaurants extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      restaurant: ""
+      restaurant: [""],
+      switch: false
     };
+    fetch(
+      `https://developers.zomato.com/api/v2.1/geocode?lat=${this.props.latitude}&lon=${this.props.longitude}&apikey=1007d1d3b051f57d7244304904aa1df2`
+    )
+      .then(res => res.json())
+      .then(json => {
+        console.log(json.nearby_restaurants);
+        this.setState({
+          restaurant: json.nearby_restaurants
+        });
+      })
+      .catch(err => err.message);
   }
 
   fetchRestaurants = () => {
@@ -19,76 +31,42 @@ class Restaurants extends Component {
         console.log(json.nearby_restaurants);
         this.setState({
           restaurant: json.nearby_restaurants
-          // name: json.nearby_restaurants.restaurant.name,
-          // address: json.nearby_restaurants.restaurant.location.address,
-          // cuisine: json.nearby_restaurants.restaurant.cuisines,
-          // rating:
-          //   json.nearby_restaurants.restaurant.user_rating.aggregate_rating,
-          // menu: json.nearby_restaurants.restaurant.menu_url,
-          // img: json.nearby_restaurants.restaurant.featured_image
         });
       })
       .then(() => {
         console.log(this.props.latitude, this.props.longitude);
         console.log(this.state.restaurant);
       })
-      .then(() => this.mapper())
       .catch(err => err.message);
   };
 
   handleSubmit = e => {
     e.preventDefault();
     this.fetchRestaurants();
+    this.setState({ switch: true });
   };
 
-  mapper = () => {
-    return this.state.restaurant.length > 0 ? (
+  switcher = () => {
+    return this.state.switch ? (
       this.state.restaurant.map(data => {
         return (
-          // (<RestaurantDisplay eatery={data.restaurant} />)
-          (<h1>{data.restaurant.name}</h1>), console.log(data.restaurant.name)
+          <div>
+            <RestaurantDisplay eatery={data} />
+          </div>
         );
       })
     ) : (
-      <h2>there are no restaurants in the area</h2>
-    );
-  };
-  // componentDidMount() {
-  //   setTimeout(() => (this.fetchRestaurants(), 2000));
-  // }
-  // const Restaurant = props => {
-
-  // const mediaImage = () => {
-  //   return item.featured_image.length > 0
-  //     ? item.featured_image
-  //     : "../../Assets/eat.jpeg";
-  // };
-  render() {
-    return (
       <div>
         <form onSubmit={e => this.handleSubmit(e)}>
           <Button type="submit">Click me!</Button>
         </form>
         <br />
-        {this.mapper}
-
-        {/* {this.state.restaurant.map(item => {
-          return console.log(item); */}
-        {/* <Card>
-            <CardHeader>{item.restaurant.name}</CardHeader>
-            <CardMedia src={item.restaurant.featured_image} />
-            <CardContent>{item.restaurant.location.address}</CardContent>
-            <CardContent>{item.restaurant.cuisines}</CardContent>
-            <CardContent>
-              {item.restaurant.user_rating.aggregate_rating}
-            </CardContent>
-            <Button href={item.restaurant.menu_url} target="blank">
-              Menu
-            </Button>
-          </Card>
-        })} */}
       </div>
     );
+  };
+
+  render() {
+    return <div>{this.switcher()}</div>;
   }
 }
 
